ff55fd6d32e306e7cbf8b2835db8285449baa11c
coby <coby5502@gmail.com>
2024-08-30 17:07:57 +0900

    Initial Commit

---
+++ CuteGirls/CuteGirlsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CuteGirlsApp.swift
+//  CuteGirls
+//
+//  Created by Coby Kim on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct CuteGirlsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
